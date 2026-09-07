c8423b51a06e41f9e99b444fbd18eba8e0b90f86
Chen <iamsbb2@qq.com>
2026-09-07 03:48:14 +0000

    refactor(ui): use local cn

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { cn } from "cn"
+import { cn } from "@/lib/utils"
 import { Select as SelectPrimitive } from "radix-ui"
 import { IconSelector, IconCheck, IconChevronUp, IconChevronDown } from "@tabler/icons-react"
 
